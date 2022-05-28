5440baf395892a794dd467ccfd717636feb19b02
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2022-05-28 23:06:58 +0530

    Add files via upload

---
+++ sizeof.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(){
+    printf("%d",sizeof(int));
+    printf("%d",sizeof(float));
+    printf("%d",sizeof(char));
+return 0;
+}
